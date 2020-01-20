ecb0583cf6c672cd285cd050a9be9f74aba68bfe
Sam Lord <sam@roweit.co.uk>
2020-01-20 09:59:51 +0000

    Added console output filtering

--- README.md
+++ README.md
@@ -11,7 +11,8 @@ Simple proxy in js
 # Usage
 
 ```cmd
-> node .\proxy.js <port>
+> node .\proxy.js <port> <regex>
 ```
 
-Port is optional, and defaults to `8012` if unset.
+- Port is optional, and defaults to `8012` if unset.
+- Regex is optional and defaults to all (`.*`). This is used to filter what urls are printed to console.
--- proxy.js
+++ proxy.js
@@ -9,23 +9,35 @@ var url = require("url");
 var net = require('net');
 var port = 8012;
 
+let regex = ".*";
 if(process.argv.length > 2) {
   port = Number(process.argv[2]);
+  if(process.argv.length > 3) {
+    regex = process.argv[3];
+  }
 }
+
+console.log("Will print matches to '" + regex + "'");
+regex = new RegExp(regex);
 
 var server = http.createServer(function (req, res) {
   var urlObj = url.parse(req.url);
   var target = urlObj.protocol + "//" + urlObj.host;
 
-  console.log("Proxy HTTP request for:", req.url);
-
   var proxy = httpProxy.createProxyServer({});
   proxy.on("error", function (err, req, res) {
     console.log("proxy error", err, req.url);
+    let message = err.toString() + " " + req.url;
+    res.statusCode = 500;
+    res.setHeader("Content-Length", Buffer.byteLength(message));
+    res.write(message);
     res.end();
   });
 
   proxy.web(req, res, {target: target});
+
+  if (regex.test(req.url)) console.log("HTTP - ", req.url);
+  console.log(req.headers["backend"]);
 }).listen(port);  //this is the port your clients will connect to
 console.log("Ready on :", port, "...");
 
@@ -50,7 +62,8 @@ server.addListener('connect', function (req, socket, bodyhead) {
   var hostPort = getHostPortFromString(req.url, 443);
   var hostDomain = hostPort[0];
   var port = parseInt(hostPort[1]);
-  console.log("Proxying HTTPS request for:", req.url);
+  
+  if (regex.test(req.url)) console.log("HTTPS - ", req.url);
 
   var proxySocket = new net.Socket();
   proxySocket.connect(port, hostDomain, function () {
